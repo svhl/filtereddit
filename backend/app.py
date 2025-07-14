c472b5d886b435d8f3c914daee8b29606aa3a271
svhl <145035644+svhl@users.noreply.github.com>
2025-07-14 22:32:58 +0530

    update backend

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import joblib
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/dist', static_url_path='')
 CORS(app)
 
 # Load your model (make sure model.pkl is in the same folder)
 model = joblib.load('model.pkl')
+
+@app.route('/', methods=['GET'])
+def home():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>', methods=['GET'])
+def static_proxy(path):
+    return send_from_directory(app.static_folder, path)
 
 @app.route('/classify', methods=['POST'])
 def classify():
@@ -22,4 +31,5 @@ def predict():
 	return jsonify({'categories': predictions.tolist()})
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
